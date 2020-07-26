5415dfdffb9152c82da56ab98b4dd315c774abf7
Andre <info@pcgames.lt>
2020-07-26 14:30:26 +0300

    Добавляем анимацию блоков

--- source/js/modules/animation.js
+++ source/js/modules/animation.js
@@ -16,4 +16,33 @@ window.addEventListener(`load`, () => {
 
   // Смена состояния псевдоэлементов меню начинается после появления меню
   setTimeout(() => body.classList.add(`menu-loaded`), 500);
+
+  // СТРАНИЦА ПРАВИЛ ---
+
+  const rulesScreen = document.querySelector(`.screen--rules`);
+  const lastRuleText = document.querySelector(`.rules__item:last-child p`);
+  const rulesLink = document.querySelector(`.rules__link`);
+
+  const mutationConfig = {
+    attributes: true,
+    childList: false,
+    subtree: false
+  };
+
+  const callback = function (mutationsList) {
+    for (let mutation of mutationsList) {
+      if (mutation.type === `attributes`) {
+        if ((rulesScreen.classList.contains(`active`)) && (rulesLink.classList.contains(`rules__link--visible`))) {
+          rulesLink.classList.remove(`rules__link--visible`);
+        }
+      }
+    }
+  };
+
+  const observer = new MutationObserver(callback);
+  observer.observe(rulesScreen, mutationConfig);
+
+  lastRuleText.addEventListener(`animationend`, () => {
+    rulesLink.classList.add(`rules__link--visible`);
+  });
 });
